2b8988d98969c1b5a7194846f7d172e57b1b06fd
BuriKizilkaya <burak.kizilkaya@outlook.com>
2024-10-08 21:30:27 +0000

    update error enum with handler enums..but not sure this is correct

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,17 @@
-#[derive(Debug, thiserror::Error)]
+use thiserror::Error;
+
+#[derive(Debug, Error)]
 pub enum Error {
     #[error("Handler not found")]
     HandlerNotFound,
 
     #[error("Serialization error")]
     SerializationError,
+
+    #[error(transparent)]
+    Other(#[from] Box<dyn std::error::Error + Sync + Send + 'static>),
+
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
--- tests/simple_ping_pong_request_response.rs
+++ tests/simple_ping_pong_request_response.rs
@@ -1,3 +1,4 @@
+use medi_rs::error::Error;
 use medi_rs::traits::IntoReq;
 use medi_rs::{bus::BusBuilder, error::Result};
 use serde::{Deserialize, Serialize};
@@ -44,13 +45,41 @@ async fn send_should_return_correct_return_values_when_multithreading() {
     }
 }
 
+#[tokio::test]
+async fn send_should_return_error_when_handler_returns_error() {
+    let bus = BusBuilder::default().add_req_handler(print_ping_with_error).build();
+
+    let result = bus.send(Ping("Ping".to_string())).await;
+
+    assert!(result.is_err());
+    let err = result.unwrap_err();
+    match &err {
+        Error::Other(err) => {
+            let err = err.downcast_ref::<PingPongError>().unwrap();
+            assert_eq!(err, &PingPongError::Error1);
+        }
+        _ => panic!("Error type is not correct"),
+    }
+    assert_eq!(err.to_string(), "Error");
+}
+
 fn print_ping(id: Ping) -> Result<Pong> {
     Ok(Pong(format!("Pong: {}", id.0)))
+}
+
+fn print_ping_with_error(_id: Ping) -> Result<Pong> {
+    Err(Error::Other(Box::new(PingPongError::Error1)))
+}
+
+#[derive(Debug, thiserror::Error, PartialEq)]
+enum PingPongError {
+    #[error("Error")]
+    Error1,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Ping(String);
 impl IntoReq<Pong> for Ping {}
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Pong(String);
