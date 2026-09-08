5321a5518c7126dd8f0f430b4db23aa659b28673
BuriKizilkaya <burak.kizilkaya@outlook.com>
2026-09-08 22:17:17 +0200

    feat: refactor event queue implementation for Tokio and Wasm adapters

--- src/medi-rs/src/adapters/mod.rs
+++ src/medi-rs/src/adapters/mod.rs
@@ -1,5 +1,62 @@
 //! Runtime queue and task-spawn adapters for generated mediators.
 
+#[allow(unused_macros)] // No runtime adapter is enabled in the default build.
+macro_rules! impl_event_queue {
+    (
+        $queue:ident,
+        sender: $sender:ty,
+        sender_binding: $sender_name:ident [$($sender_mut:tt)*],
+        receiver: $receiver:ty,
+        channel: |$capacity:ident| $channel:expr,
+        receive: |$receiver_name:ident| $receive:expr $(,)?
+    ) => {
+        pub struct $queue<T> {
+            sender: Mutex<($sender, bool)>,
+            receiver: Mutex<$receiver>,
+        }
+
+        impl<T: Send + 'static> crate::EventQueue<T> for $queue<T> {
+            fn new($capacity: Option<usize>) -> Self {
+                let (sender, receiver) = $channel;
+                Self {
+                    sender: Mutex::new((sender, false)),
+                    receiver: Mutex::new(receiver),
+                }
+            }
+
+            async fn publish(&self, item: T) -> crate::Result<()> {
+                let $($sender_mut)* $sender_name = self.sender.lock().await;
+                if $sender_name.1 {
+                    return Err(crate::Error::EventPublishingError);
+                }
+                $sender_name
+                    .0
+                    .send(item)
+                    .await
+                    .map_err(|_| crate::Error::EventPublishingError)
+            }
+
+            async fn close(&self) {
+                self.sender.lock().await.1 = true;
+            }
+
+            async fn publish_internal(&self, item: T) -> crate::Result<()> {
+                let $($sender_mut)* $sender_name = self.sender.lock().await;
+                $sender_name
+                    .0
+                    .send(item)
+                    .await
+                    .map_err(|_| crate::Error::EventPublishingError)
+            }
+
+            async fn recv(&self) -> crate::Result<T> {
+                let mut $receiver_name = self.receiver.lock().await;
+                ($receive).ok_or(crate::Error::EventProcessingError)
+            }
+        }
+    };
+}
+
 pub mod lifecycle;
 pub mod queue;
 pub mod shutdown;
@@ -11,16 +68,16 @@ pub mod tokio;
 #[cfg(feature = "wasm")]
 pub mod wasm;
 
-#[cfg(feature = "tokio")]
+#[cfg(all(feature = "tokio", not(feature = "wasm"), not(feature = "embassy")))]
 pub mod selected {
-    pub use super::tokio::{TokioEventQueue as EventQueue, spawn};
+    pub use super::tokio::TokioEventQueue as EventQueue;
+    pub use super::tokio::spawn;
 }
-
-#[cfg(all(feature = "wasm", not(feature = "tokio")))]
+#[cfg(all(feature = "wasm", not(feature = "tokio"), not(feature = "embassy")))]
 pub mod selected {
-    pub use super::wasm::{WasmEventQueue as EventQueue, spawn};
+    pub use super::wasm::WasmEventQueue as EventQueue;
+    pub use super::wasm::spawn;
 }
-
 #[cfg(all(feature = "embassy", not(feature = "tokio"), not(feature = "wasm")))]
 pub mod selected {
     pub use super::embassy::EmbassyEventQueue as EventQueue;
